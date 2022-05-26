78cfd39ae6ff46143c063b8d9c18bd085fc27264
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2022-05-26 09:14:03 -0400

    first page with nextjs

---
+++ pages/faq.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function FAQ() {
+  return (
+    <div>
+      <h1>FAQ</h1>
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+    </div>
+  );
+}
