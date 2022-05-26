1c647d1d3370ad41cc845c9101e3b1d39c27d956
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2022-05-26 11:23:17 -0400

    getting elements with getstaticprops and using datafeching

--- pages/store/index.js
+++ pages/store/index.js
@@ -1,9 +1,19 @@
 import Layout from "../../components/layout";
+import { getItems } from "../../service/itemService";
 
-export default function Index() {
+export default function Index({ items }) {
   return (
-   <Layout>
-       Store
-   </Layout>
-  )
+    <Layout>
+      <h1>Store</h1>
+      {items && items.map((item) => <div key={item.id}>{item.title}</div>)}
+    </Layout>
+  );
+}
+export async function getStaticProps() {
+  const res = await getItems();
+  return {
+    props: {
+      items: res,
+    },
+  };
 }
