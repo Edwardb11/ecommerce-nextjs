97da63e907ba2bfbccf6197615830eb70dff2740
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2022-05-27 15:51:36 -0400

    adding the functions

--- components/stateWrapper.js
+++ components/stateWrapper.js
@@ -1,12 +1,41 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 const AppContext = createContext({
-  isOpen: true,
+  isOpen: false,
   items: [],
   openCart: () => {},
+  closeCart: () => {},
   addItemToCart: () => {},
   getNumberOfItems: () => {},
 });
 export default function StateWrapper({ children }) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [items, setItems] = useState([]);
+
+  function handleOpenCart() {
+    setIsOpen(true);
+  }
+  function handleCloseCart() {
+    setIsOpen(false);
+  }
+
+  function handleAddItemToCart(item) {
+    const temp = [...items];
+    const found = temp.find((product) => product.id === item.id);
+
+    if (found) {
+      found.qty++;
+    } else {
+      item.qty = 1;
+    }
+
+    setItems([...temp]);
+  }
+
+  function handleNumberOfItems() {
+    total = items.reduce((acc, item) => acc + item.qty, 0);
+    return total;
+  }
+  
   return <div>{children}</div>;
 }
